42cdeb5278248efa2defd24e982d7deb1c5ab27b
yindaheng98 <yindaheng98@gmail.com>
2025-08-08 23:14:17 -0700

    collect

---
+++ collect.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir -p Ours
+root=output
+for folder in output/*-nocam0-gscompress; do
+    video=$(basename "${folder%-nocam0-gscompress}")
+    echo "Processing $video"
+    cp output/$video-nocam0-gscompress/frame1/ours_30000/quality.csv Ours/$video.csv
+    cp output/$video-nocam0-gscompress/frame1/decompress.log Ours/$video.log
+done
