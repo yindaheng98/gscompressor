a8f1799bdf88252ebcd283f82be35807ad8277d3
yindaheng98 <yindaheng98@gmail.com>
2026-01-28 15:05:24 -0800

    test eq

--- equivalence.sh
+++ equivalence.sh
@@ -5,14 +5,16 @@ SOURCE_DIR="output/truck"
 ITERATION="30000"
 SH_DEGREE="3"
 TEMP_DIR="output/truck-gscompress"
+ENC="python -m gscompressor.compress --sh_degree $SH_DEGREE -s $SOURCE_DIR -i $ITERATION compress"
+DEC="python -m gscompressor.compress --sh_degree $SH_DEGREE -s $SOURCE_DIR -i $ITERATION decompress"
 
 echo "Source: $SOURCE_DIR, Iteration: $ITERATION"
 
 # 1. exe encode
-python -m gscompressor.compress --sh_degree "$SH_DEGREE" -s "$SOURCE_DIR" -d "$TEMP_DIR/exe" -i "$ITERATION" compress --use_executable_backend
+$ENC -d "$TEMP_DIR/exe" --use_executable_backend
 
 # 2. pyd encode
-python -m gscompressor.compress --sh_degree "$SH_DEGREE" -s "$SOURCE_DIR" -d "$TEMP_DIR/pyd" -i "$ITERATION" compress
+$ENC -d "$TEMP_DIR/pyd"
 
 # compare drc
 echo "=== DRC MD5 ==="
@@ -29,22 +31,22 @@ cp "$TEMP_DIR/pyd/point_cloud/iteration_$ITERATION/point_cloud.drc" "$TEMP_DIR/p
 cp "$TEMP_DIR/pyd/point_cloud/iteration_$ITERATION/point_cloud.drc" "$TEMP_DIR/pyd_pyd/point_cloud/iteration_$ITERATION/"
 
 # 3. exe encode -> exe decode
-python -m gscompressor.compress --sh_degree "$SH_DEGREE" -s "$SOURCE_DIR" -d "$TEMP_DIR/exe_exe" -i "$ITERATION" decompress --use_executable_backend
+$DEC -d "$TEMP_DIR/exe_exe" --use_executable_backend
 
 # 4. exe encode -> pyd decode
-python -m gscompressor.compress --sh_degree "$SH_DEGREE" -s "$SOURCE_DIR" -d "$TEMP_DIR/exe_pyd" -i "$ITERATION" decompress
+$DEC -d "$TEMP_DIR/exe_pyd"
 
 # 5. pyd encode -> exe decode
-python -m gscompressor.compress --sh_degree "$SH_DEGREE" -s "$SOURCE_DIR" -d "$TEMP_DIR/pyd_exe" -i "$ITERATION" decompress --use_executable_backend
+$DEC -d "$TEMP_DIR/pyd_exe" --use_executable_backend
 
 # 6. pyd encode -> pyd decode
-python -m gscompressor.compress --sh_degree "$SH_DEGREE" -s "$SOURCE_DIR" -d "$TEMP_DIR/pyd_pyd" -i "$ITERATION" decompress
+$DEC -d "$TEMP_DIR/pyd_pyd"
 
 # 比较ply
 echo "=== PLY MD5 ==="
 md5sum "$TEMP_DIR/exe_exe/point_cloud/iteration_$ITERATION/point_cloud.ply"
+md5sum "$TEMP_DIR/pyd_pyd/point_cloud/iteration_$ITERATION/point_cloud.ply"
 md5sum "$TEMP_DIR/exe_pyd/point_cloud/iteration_$ITERATION/point_cloud.ply"
 md5sum "$TEMP_DIR/pyd_exe/point_cloud/iteration_$ITERATION/point_cloud.ply"
-md5sum "$TEMP_DIR/pyd_pyd/point_cloud/iteration_$ITERATION/point_cloud.ply"
 
 rm -rf "$TEMP_DIR"
